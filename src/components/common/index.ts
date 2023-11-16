c03c66a56defafd5150861e52cf1eca62ec02226
Sai Min Pyae Kyaw <saiminpyaekyaw@gmail.com>
2023-11-16 07:51:28 +0630

    remove logo

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,7 +1,6 @@
 export * from './Amount';
 export * from './Card';
 export * from './Can';
-export * from './Logo';
 export * from './ProfileAvatar';
 export * from './Search';
 export * from './LogoSlogan';
