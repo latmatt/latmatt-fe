87fd1a9a859fd699e781179be3b65d10793ad78b
Sai Min Pyae Kyaw <saiminpyaekyaw@gmail.com>
2024-02-04 20:55:43 +0630

    added ticket row and seats

--- src/pages/event-detail/components/TicketCounter/TicketZone.tsx
+++ src/pages/event-detail/components/TicketCounter/TicketZone.tsx
@@ -1,9 +1,68 @@
-import { Box, Text } from '@mantine/core';
+import { Box, Grid, Space, Text } from '@mantine/core';
+import classes from './style.module.css';
+
+function Row() {
+  return (
+    <Grid.Col span={{ base: 12 / 8, sm: 12 / 8, md: 12 / 8 }}>
+      <Box className={classes.rowContainer}>
+        <Text fz="sm">1</Text>
+      </Box>
+    </Grid.Col>
+  );
+}
+
+function Seat() {
+  return (
+    <Grid.Col span={{ base: 12 / 8, sm: 12 / 8, md: 12 / 8 }}>
+      <Box className={classes.seatContainer}>
+        <Text fz="sm">1</Text>
+      </Box>
+    </Grid.Col>
+  );
+}
 
 export function TicketZone() {
   return (
     <Box>
-      <Text>Ticket Zone</Text>
+      <Box p="sm" bg="neutral.0">
+        <Text fw="bold">Rows</Text>
+
+        <Grid gutter={2}>
+          <Row />
+
+          <Row />
+
+          <Row />
+
+          <Row />
+
+          <Row />
+
+          <Row />
+        </Grid>
+      </Box>
+
+      <Space h="sm" />
+
+      <Box p="sm" bg="neutral.0">
+        <Text fw="bold">Seats</Text>
+
+        <Grid gutter={2}>
+          <Seat />
+
+          <Seat />
+
+          <Seat />
+
+          <Seat />
+
+          <Seat />
+
+          <Seat />
+        </Grid>
+      </Box>
+
+      <Space h="sm" />
     </Box>
   );
 }
